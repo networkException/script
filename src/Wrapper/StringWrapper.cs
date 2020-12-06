fe00b6e641f172f836aba5fdfbda607c0f0985ca
networkException <git@nwex.de>
2020-12-06 22:19:17 +0100

    StringWrapper: Implemented substring and sublength

--- src/Wrapper/StringWrapper.cs
+++ src/Wrapper/StringWrapper.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace networkScript.Wrapper {
 	public class StringWrapper : Object {
 		private readonly string m_value;
@@ -6,10 +8,40 @@ namespace networkScript.Wrapper {
 			m_value = value.asString();
 
 			set("length", length);
+			set("substring", substring);
+			set("sublength", sublength);
 		}
 
-		private Value length() {
-			return new Value(m_value.Length);
+		private Value length() { return new Value(m_value.Length); }
+
+		private Value substring(IReadOnlyList<Expression> parameters, Context context) {
+			if (parameters.Count != 1 || parameters.Count != 2) context.typeError("Expected 1-2 arguments for StringWrapper.substring");
+
+			if (parameters.Count == 1) {
+				int first = (int) parameters[0].evaluate(context).asDouble();
+				
+				return new Value(m_value.Substring(first));
+			} else {
+				int first = (int) parameters[0].evaluate(context).asDouble();
+				int second = (int) parameters[0].evaluate(context).asDouble();
+				
+				return new Value(m_value.Substring(first, m_value.Length - second));
+			}
+		}
+
+		private Value sublength(IReadOnlyList<Expression> parameters, Context context) {
+			if (parameters.Count != 1 || parameters.Count != 2) context.typeError("Expected 1-2 arguments for StringWrapper.sublength");
+
+			if (parameters.Count == 1) {
+				int first = (int) parameters[0].evaluate(context).asDouble();
+				
+				return new Value(m_value.Substring(0, first));
+			} else {
+				int first = (int) parameters[0].evaluate(context).asDouble();
+				int second = (int) parameters[0].evaluate(context).asDouble();
+				
+				return new Value(m_value.Substring(first, second));
+			}
 		}
 
 		public override string ToString() { return "StringWrapper(" + m_value + ")"; }
