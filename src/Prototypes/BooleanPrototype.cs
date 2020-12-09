190887f8ea5cfa677084fc5e8ec77c0ef82ca6b8
networkException <git@nwex.de>
2020-12-09 21:53:55 +0100

    BooleanPrototype: Implemented invert function

--- src/Prototypes/BooleanPrototype.cs
+++ src/Prototypes/BooleanPrototype.cs
@@ -1,8 +1,18 @@
+using System.Collections.Generic;
+
 namespace networkScript.Prototypes {
 	public class BooleanPrototype : Object {
 		private readonly bool m_value;
 
-		public BooleanPrototype(bool value) { m_value = value; }
+		public BooleanPrototype(bool value) {
+			m_value = value;
+
+			set("invert", invert);
+		}
+
+		private Value invert(IReadOnlyList<Expression> parameters, Context context) {
+			return new Value(!m_value);
+		}
 
 		public override string ToString() { return m_value ? "true" : "false"; }
 	}
